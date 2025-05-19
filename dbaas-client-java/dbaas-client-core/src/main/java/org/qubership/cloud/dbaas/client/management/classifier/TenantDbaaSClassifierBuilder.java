d4982ab764d528a976807a3a1b17c527a8985a3b
larkin <larkin.qubership@gmail.com>
2025-05-19 16:20:57 +0400

    fix 4

--- dbaas-client-java/dbaas-client-core/src/main/java/org/qubership/cloud/dbaas/client/management/classifier/TenantDbaaSClassifierBuilder.java
+++ dbaas-client-java/dbaas-client-core/src/main/java/org/qubership/cloud/dbaas/client/management/classifier/TenantDbaaSClassifierBuilder.java
@@ -4,10 +4,7 @@ import org.qubership.cloud.framework.contexts.tenant.TenantContextObject;
 import org.qubership.cloud.context.propagation.core.ContextManager;
 import org.qubership.cloud.dbaas.client.management.DbaasDbClassifier;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-import static org.qubership.cloud.framework.contexts.tenant.TenantProvider.TENANT_CONTEXT_NAME;
 import static org.qubership.cloud.dbaas.client.DbaasConst.*;
 
 
@@ -25,7 +22,7 @@ public class TenantDbaaSClassifierBuilder extends DbaaSChainClassifierBuilder {
     public DbaasDbClassifier build() {
         return new DbaasDbClassifier.Builder()
                 .withProperties(super.build().asMap())
-                .withProperty(TENANT_ID, ((TenantContextObject) ContextManager.get(TENANT_CONTEXT_NAME)).getTenant())
+                .withProperty(TENANT_ID, ((TenantContextObject) ContextManager.get("tenant")).getTenant())
                 .withProperty(SCOPE, TENANT)
                 .build();
     }
